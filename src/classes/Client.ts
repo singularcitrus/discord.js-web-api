a67b2a7272e036a4ca511f6d884dbe8c52f5eddf
Lourens Ros <lourensros@gmail.com>
2021-07-28 14:51:42 +0200

    Move default route setup to private function

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -32,15 +32,14 @@ export class Client extends Discord.Client {
     this.appOptions = completeOptions(appOptions);
     this.express = express();
     this.express.defaultRouter = Router();
-    this.express.defaultRouter.all("/lib", (req, res) =>
-      res.send("discord.js-web-api")
-    );
     this.express.use(this.appOptions.rootPath, this.express.defaultRouter);
 
     // Load middlewares required
     this.express.use(Cors());
     this.express.use(BodyParser.urlencoded({ extended: false }));
     this.express.use(BodyParser.json());
+
+    this._defaultRoutes();
 
     // Set up auto startup if required
     if (this.appOptions.autoStartup) {
@@ -49,6 +48,12 @@ export class Client extends Discord.Client {
         this.spawnExpressProcess();
       });
     }
+  }
+
+  private _defaultRoutes() {
+    this.express.defaultRouter?.all("/lib", (req, res) =>
+      res.send("discord.js-web-api")
+    );
   }
 
   // A function to start up express
