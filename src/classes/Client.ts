75fb3f92ebd451f24ba5bf279a6bfbf5bc9ba1a0
Lourens Ros <lourensros@gmail.com>
2021-07-28 14:06:42 +0200

    Add rootpath feature

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -1,16 +1,23 @@
 import Discord, { ClientOptions } from "discord.js";
-import express, { Express } from "express";
+import express, { Express, Router } from "express";
+import BodyParser from "body-parser";
+import Cors from "cors";
 import { defaultLogger } from "../";
 
 export class Client extends Discord.Client {
   private appOptions: _CompleteOptions;
-  public express: Express;
+  public express: _extendedExpress;
 
   constructor(discordOptions: ClientOptions, appOptions: AppOptions) {
     super(discordOptions);
 
     this.appOptions = completeOptions(appOptions);
     this.express = express();
+    this.express.defaultRouter = Router();
+    this.express.defaultRouter.all("/lib", (req, res) =>
+      res.send("discord.js-web-api")
+    );
+    this.express.use(this.appOptions.rootPath, this.express.defaultRouter);
 
     // Load middlewares required
     this.express.use(Cors());
@@ -29,7 +36,9 @@ export class Client extends Discord.Client {
   // A function to start up express
   spawnExpressProcess() {
     this.express.listen(this.appOptions.port, () => {
-      defaultLogger.log(`Express started on port ${this.appOptions.port}`);
+      defaultLogger.log(
+        `Express started on http://localhost:${this.appOptions.port}${this.appOptions.rootPath}`
+      );
     });
   }
 }
@@ -37,6 +46,7 @@ export class Client extends Discord.Client {
 export interface AppOptions {
   port?: number;
   autoStartup?: boolean;
+  rootPath?: string;
 }
 
 // Internal function to complete all the provided options with their default values
@@ -44,10 +54,18 @@ function completeOptions(options: AppOptions): _CompleteOptions {
   return {
     port: options.port ? options.port : 8080,
     autoStartup: !!options.autoStartup,
+    rootPath: options.rootPath
+      ? `${options.rootPath.startsWith("/") ? "" : "/"}${options.rootPath}`
+      : "",
   };
 }
 
 interface _CompleteOptions extends AppOptions {
   port: number;
   autoStartup: boolean;
+  rootPath: string;
+}
+
+interface _extendedExpress extends Express {
+  defaultRouter?: Router;
 }
