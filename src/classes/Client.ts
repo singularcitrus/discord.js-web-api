3353343de0711c1b2fa81fb59046727e272cd3bc
Lourens Ros <lourensros@gmail.com>
2021-07-30 09:45:32 +0200

    Prevent users from adding routes directly on express, require the use of express.defaultRouter instead

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -50,6 +50,9 @@ export class Client extends Discord.Client {
     // Load default routes
     this._defaultRoutes();
 
+    // Proceed to lock users from modifying default express router
+    this._lockExpress();
+
     // Set up auto startup if required
     if (this.appOptions.autoStartup) {
       this.on("ready", () => {
@@ -72,6 +75,19 @@ export class Client extends Discord.Client {
     }
   }
 
+  private _lockExpress() {
+    // @ts-ignore
+    this.express.all = _expressLocked;
+    // @ts-ignore
+    this.express.use = _expressLocked;
+    // @ts-ignore
+    this.express.get = _expressLocked;
+    // @ts-ignore
+    this.express.post = _expressLocked;
+    // @ts-ignore
+    this.express.put = _expressLocked;
+  }
+
   // A function to start up express
   spawnExpressProcess() {
     this.express.listen(this.appOptions.port, () => {
@@ -80,6 +96,12 @@ export class Client extends Discord.Client {
       );
     });
   }
+}
+
+function _expressLocked(): void {
+  throw Error(
+    "Trying to modify root express router use `express.defaultRouter` instead"
+  );
 }
 
 // Internal function to complete all the provided options with their default values
