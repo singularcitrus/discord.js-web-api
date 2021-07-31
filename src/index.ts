784efb2fb5d963f7d028d69e7068868bcd17f013
Lourens Ros <lourensros@gmail.com>
2021-07-31 10:10:26 +0200

    Actually export the "Authenticated" middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./types";
 export * from "./classes/Client";
 
+export * as Authenticated from "./middleware/Authenticated";
+
 export var defaultLogger: any = console;
 
 export function OverrideDefaultSysOut(newValue: any) {
